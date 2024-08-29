9e0dd8ad5c4675cead536bddf5eafb0a6930686d
Dimitri Kobassian <d.kobassian@gmail.com>
2024-08-29 18:35:03 +0200

    setting up the workspace

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import '../styles/Header.scss';
+import logo from '../assets/logo.svg';
+
+function Header() {
+  return (
+    <header className="Header">
+      <img></img>
+    </header>
+  );
+}
+
+export default Header;
