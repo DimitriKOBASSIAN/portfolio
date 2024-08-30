3d82b6b0b9add77313887ea11e9b7ce37de614e3
Dimitri Kobassian <d.kobassian@gmail.com>
2024-08-31 01:26:53 +0200

    some styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <header className="Header">
       <h1 className="Header-left">
-<Logo className="Header-logo" alt="logo" /> <span className="Header-span">Dimitri KOBASSIAN</span></h1>
+<Logo className="Header-logo" alt="logo" /> <p className="Header-span">Dimitri KOBASSIAN</p></h1>
 <div className="Header-right"><Navbar /></div>
     </header>
 
