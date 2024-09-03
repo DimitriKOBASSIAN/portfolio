09bd0a7d4b0aaff8cbaf66b0e95f00ffcdc8b142
Dimitri Kobassian <d.kobassian@gmail.com>
2024-09-03 13:42:27 +0200

    more more responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,12 +12,28 @@ function Navbar() {
         setIsOpen(false);
     };
 
-    const scrollToTop = () => {
+    const scrollToTop = (event) => {
+        event.preventDefault();
         window.scrollTo({
             top: 0,
             behavior: 'smooth'
         });
         closeMenu(); // Close the menu after scrolling
+    };
+
+    const scrollToSection = (event, id, offset = 0) => {
+        event.preventDefault();
+        const element = document.getElementById(id);
+        if (element) {
+            const elementPosition = element.getBoundingClientRect().top + window.pageYOffset;
+            const offsetPosition = elementPosition - offset;
+
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: 'smooth'
+            });
+            closeMenu(); // Close the menu after scrolling
+        }
     };
 
     return (
@@ -26,14 +42,14 @@ function Navbar() {
                 &#9776; {/* Hamburger icon */}
             </div>
             <ul className={`Navbar-list ${isOpen ? 'open' : ''}`}>
-                <li className="Navbar-list-item">
-                    <a href="#home" onClick={scrollToTop}>Home</a>
+                <li className="Navbar-list-item" onClick={(event) => scrollToTop(event)}>
+                    <a href="#home">Home</a>
                 </li>
-                <li className="Navbar-list-item">
-                    <a href="#cta" onClick={closeMenu}>About</a>
+                <li className="Navbar-list-item" onClick={(event) => scrollToSection(event, 'cta', 50)}>
+                    <a href="#cta">About</a>
                 </li>
-                <li className="Navbar-list-item">
-                    <a href="#projects" onClick={closeMenu}>Projects</a>
+                <li className="Navbar-list-item" onClick={(event) => scrollToSection(event, 'projects', 50)}>
+                    <a href="#projects">Projects</a>
                 </li>
             </ul>
         </div>
