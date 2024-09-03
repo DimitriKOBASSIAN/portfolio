5b974274128ad977da74df4ecffd6803225df632
Dimitri Kobassian <d.kobassian@gmail.com>
2024-09-03 13:08:48 +0200

    responsive almost OK

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,4 +17,3 @@ function Header() {
 }
 
 export default Header;
-<Logo className="Header-logo" alt="logo" /> 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Navbar.scss';
 
 function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const closeMenu = () => {
+        setIsOpen(false);
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+        closeMenu(); // Close the menu after scrolling
+    };
+
     return (
         <div className="Navbar">
-            <ul className="Navbar-list">
+            <div className="Navbar-toggle" onClick={toggleMenu}>
+                &#9776; {/* Hamburger icon */}
+            </div>
+            <ul className={`Navbar-list ${isOpen ? 'open' : ''}`}>
                 <li className="Navbar-list-item">
-                    <a href="#home">Home</a>
+                    <a href="#home" onClick={scrollToTop}>Home</a>
                 </li>
                 <li className="Navbar-list-item">
-                    <a href="#cta">About</a>
+                    <a href="#cta" onClick={closeMenu}>About</a>
                 </li>
                 <li className="Navbar-list-item">
-                    <a href="#projects">Projects</a>
+                    <a href="#projects" onClick={closeMenu}>Projects</a>
                 </li>
-                {/* <li className="Navbar-list-item">Contact</li> */}
             </ul>
         </div>
     );
